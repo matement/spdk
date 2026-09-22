dd7e4fb0f833e78b07b9a5955b387a557e5abc8d
matement <matement>
2026-09-22 03:24:34 +0300

    latest working build

--- lib/nvme/gpu_funcs.h
+++ lib/nvme/gpu_funcs.h
@@ -1,7 +1,13 @@
 #ifndef GPU_SPDK_HEADER_FILE
 #define GPU_SPDK_HEADER_FILE
 
+#include <stdint.h>
 #include <cuda_runtime_api.h>
+
+
+#ifdef __cplusplus
+extern "C"
+#endif
 void gpu_mmio_write(volatile uint32_t *dbl, uint32_t val);
 
 #endif
